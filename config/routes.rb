a716b9cf7ddc6d448f1945a09a47357ac0a45d8d
Hao Lam <tuonghao2001@gmail.com>
2022-03-08 19:52:12 -0800

    add forgotPassword and signUpConfirmation from devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  #pins
   resources :pins
+  
   #devise
   devise_for :users
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_09_002933) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_09_033220) do
   create_table "pins", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", null: false
@@ -25,6 +25,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_09_002933) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
